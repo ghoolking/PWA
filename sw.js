ca542b3a270d2488d4369520cda57448dc1ee1f1
chenronhui <zxc110330@outlook.com>
2022-02-23 15:24:16 +0800

    add qrcode

--- sw.js
+++ sw.js
@@ -1,12 +1,11 @@
 // importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox-sw.js");
-var cacheStorageKey = 'minimal-pwa-4'
+var cacheStorageKey = 'minimal-pwa-5'
 var cacheList = [
-  '/PWA/index.html',
-  '/PWA/index.css',
-  '/PWA/icons.png',
-  '/PWA/qrcode.js',
-  '/PWA/qrcode.min.js',
-  '/PWA/jquery.min.js'
+  'index.html',
+  'index.css',
+  'icons.png',
+  'jquery.min.js',
+  'qrcode.js'
 ]
 self.addEventListener('install', e => {
   e.waitUntil(
